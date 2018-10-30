f757b863056167d55edd09d1dd06ce39b2adf41d
jason <jasontuburan@gmail.com>
2018-10-30 08:40:35 +0800

    react-bootstrap installed

---
+++ src/header/nav-bar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button bsStyle="primary">Primary</Button>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
